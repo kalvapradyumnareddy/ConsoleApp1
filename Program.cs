61096bdbc8df22c70c2b60a468c2e198a7d04b5e
pradyumna reddy <60683250+kalvapradyumnareddy@users.noreply.github.com>
2021-05-05 16:58:29 +0530

    first change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello this is the first change !");
         }
     }
 }
